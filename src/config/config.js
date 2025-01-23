8aa9a3cbc0a7feb72d996ed97b2a3d455c45a588
kratos606 <yassirbenmoussa84@gmail.com>
2025-01-23 19:14:14 +0100

    FIXED CONFLICT IN FUNCTIONS WITH REACT

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const CDN_URL = 'https://cdn.statically.io/gh/ardisplaycorp/bridge/v1.1.3/build';
+export const CDN_URL = 'https://cdn.statically.io/gh/ardisplaycorp/bridge/v1.1.4/build';
 export const BRIDGE_URL = 'http://localhost:5173';
