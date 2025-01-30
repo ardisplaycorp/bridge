f551aafc5a58554352d02e391589aa527427f553
kratos606 <yassirbenmoussa84@gmail.com>
2025-01-30 22:13:25 +0100

    v1.3.2

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const CDN_URL =
-  "https://cdn.statically.io/gh/ardisplaycorp/bridge/v1.3.0/build";
+  "https://cdn.statically.io/gh/ardisplaycorp/bridge/v1.3.2/build";
 export const BRIDGE_URL = "https://ardisplaybridge.vercel.app";
