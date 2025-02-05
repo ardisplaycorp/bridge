c3cbda0bc03a0cf6d1db348fe0a87960004e472c
kratos606 <yassirbenmoussa84@gmail.com>
2025-02-05 07:26:36 +0100

    FIXED DIMESNSION PROBLEM RTL 3

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const CDN_URL =
-  "https://cdn.statically.io/gh/ardisplaycorp/bridge/v1.5.6/build";
+  "https://cdn.statically.io/gh/ardisplaycorp/bridge/v1.5.7/build";
 export const BRIDGE_URL = "https://ardisplaybridge.vercel.app";
